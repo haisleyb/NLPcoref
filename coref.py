43e87519320604ca919f09fc17c29d5de36afb84
Haisley Brooking <haisleybrooking@gmail.com>
2019-11-01 22:53:33 -0600

    Cleaned up parsing

--- coref.py
+++ coref.py
@@ -1,149 +1,199 @@
 import nltk
 from nltk.corpus import wordnet
-import string
+from nltk import word_tokenize, pos_tag, ne_chunk
 import re
-from nltk.tokenize import word_tokenize
+import warnings
 from nltk.corpus import stopwords
+from fuzzywuzzy import fuzz
 
+from fuzzywuzzy import process
 from nltk.stem import WordNetLemmatizer
 from nltk.parse.corenlp import CoreNLPParser
-import os
-from nltk import tree
-from nltk.parse import RecursiveDescentParser
 import sys
 
 
 class coref_file:
     def __init__(self, file_name, contents, response_dir):
         self.name = file_name.split('.')[0].split('/')[-1]
+        # Original sentences with the xml
         self.sentences = contents
-        self.corefs = []
-        self.coref_dict = {}
-        self.regular_words = []
+        # Sentences without xml
+        self.cleaned_sentences = []
+        # Dictionary with coref noun as the key and number as a value
+        self.coref_index = {}
+        # Dictionary with sentence number as a key and a list of corefs in them
+        self.coref_sentence = {}
+        # Dictionary of corefs and what they've resolved to
+        self.coref_resolved = {}
         self.START_COREF_TAG = "<COREF ID="
         self.END_COREF_TAG = "</COREF>"
         self.response = response_dir
-        self.coref_location = {}
+
+
+    def build_tag(self, head, i):
+        return self.START_COREF_TAG + '"X' + str(i) + '">' + head + self.END_COREF_TAG
 
     def find_corefs(self):
         i = 0
         sent = 0
         for sentence in self.sentences:
-            self.coref_location[sent] = []
+            self.coref_sentence[sent] = []
             start_index = -1
-            sentence_words = []
             while True:
                 start_index = sentence.find(self.START_COREF_TAG)
                 if start_index < 0:
-                    if len(sentence) > 0:
-                        if sentence not in string.punctuation:
-                            sentence_words.append(sentence)
+                    self.cleaned_sentences.append(sentence)
                     break
-                if sentence[:start_index] not in string.punctuation:
-                    sentence_words.append(sentence[:start_index])
                 temp = sentence.find(">")
                 end_index = sentence.find(self.END_COREF_TAG)
                 head_coref = sentence[start_index + (temp - start_index + 1):end_index]
-                self.corefs.append((head_coref, i))
-                self.coref_location[sent].append(head_coref)
-                sentence = sentence[end_index + 8:]
+                tag = self.build_tag(head_coref, i)
+                sentence = sentence.replace(tag, "X" + str(i))
+                self.coref_index[head_coref] = i
+                self.coref_sentence[sent].append(head_coref)
+                self.coref_resolved[head_coref] = []
                 i += 1
-            self.regular_words.append(sentence_words)
             sent += 1
+        return self.coref_index
 
-        return self.corefs
-
-    def string_match(self):
-        for loc in self.coref_location.keys():
-            heads = self.coref_location[loc]
+    def exact_string_match(self):
+        for loc in self.coref_sentence.keys():
+            # Get the list of corefs in the current sentence
+            heads = self.coref_sentence[loc]
             for noun in heads:
-                for sentence in self.regular_words[loc:]:
-                    for string in sentence:
-                        if re.search(noun, string, re.IGNORECASE):
-                            index = string.lower().find(noun.lower())
-                            matched_word = string[index:index + len(noun)]
-                            # Tuple of word and index of sentence it came from
-                            t = (matched_word, self.regular_words.index(sentence))
-                            if noun in self.coref_dict.keys():
-                                if t not in self.coref_dict[noun]:
-                                    self.coref_dict[noun].append(t)
-                            else:
-                                self.coref_dict[noun] = [t]
+                # Get the coref number
+                num = self.coref_index[noun]
+                # Only loop through the sentence the coref is in and onwards
+                for sentence in self.cleaned_sentences[loc:]:
+                    # Check if coref is in the sentence
+                    index = sentence.find("X" + str(num))
+                    if index > 0:
+                        # Find where the coref begins in the sentence
+                        sentence = sentence[index + 3:]
+                    if re.search(noun, sentence, re.IGNORECASE):
+                        index = sentence.lower().find(noun.lower())
+                        matched_word = sentence[index:index + len(noun)]
+                        # Tuple of word and sentence number it came from
+                        t = (matched_word, loc)
+                        if t not in self.coref_resolved[noun]:
+                            self.coref_resolved[noun].append(t)
 
-    def synonym_match(self):
-        for loc in self.coref_location.keys():
-            heads = self.coref_location[loc]
+    def exact_synonym_match(self):
+        for loc in self.coref_sentence.keys():
+            # Get the list of corefs in the current sentence
+            heads = self.coref_sentence[loc]
             for noun in heads:
-                synonyms = []
+                # Get the coref number
+                num = self.coref_index[noun]
                 # Build our nouns list of synonyms
+                synonyms = []
                 for syn in wordnet.synsets(noun):
                     for l in syn.lemmas():
                         if l.name() not in synonyms:
                             synonyms.append(l.name().replace("_", " "))
-                for sentence in self.regular_words[loc:]:
-                    for string in sentence:
-                        for syn in synonyms:
-                            if re.search(syn, string, re.IGNORECASE):
-                                # Case insensitive search
-                                index = string.lower().find(syn.lower())
-                                matched_word = string[index:index + len(syn)]
-                                # Tuple of word and index of sentence it came from
-                                t = (matched_word, self.regular_words.index(sentence))
-                                if noun in self.coref_dict.keys():
-                                    if t not in self.coref_dict[noun] and t[1] != self.regular_words.index(sentence):
-                                        self.coref_dict[noun].append(t)
-                                else:
-                                    self.coref_dict[noun] = [t]
-
-    def all_exact_string_match(self):
-        for noun_t in self.corefs:
-            noun = noun_t[0]
-            for sentence in self.regular_words:
-                for string in sentence:
-                    if re.search(noun, string, re.IGNORECASE):
-                        index = string.lower().find(noun.lower())
-                        matched_word = string[index:index+len(noun)]
-                        # Tuple of word and index of sentence it came from
-                        t = (matched_word, self.regular_words.index(sentence))
-                        if noun in self.coref_dict.keys():
-                            if t not in self.coref_dict[noun]:
-                                self.coref_dict[noun].append(t)
-                        else:
-                            self.coref_dict[noun] = [t]
-
-    def all_synonym_match(self):
-        for noun_t in self.corefs:
-            noun = noun_t[0]
-            synonyms = []
-            # Build our nouns list of synonyms
-            for syn in wordnet.synsets(noun):
-                for l in syn.lemmas():
-                    if l.name() not in synonyms:
-                        synonyms.append(l.name())
-            for sentence in self.regular_words:
-                for string in sentence:
+                # Only loop through the sentence the coref is in and onwards
+                for sentence in self.cleaned_sentences[loc:]:
+                    # Check if coref is in the sentence
+                    index = sentence.find("X" + str(num))
+                    if index > 0:
+                        # Find where the coref begins in the sentence
+                        sentence = sentence[index + 3:]
                     for syn in synonyms:
-                        if re.search(syn, string, re.IGNORECASE):
-                            # Case insensitive search
-                            index = string.lower().find(syn.lower())
-                            matched_word = string[index:index+len(syn)]
-                            # Tuple of word and index of sentence it came from
-                            t = (matched_word, self.regular_words.index(sentence))
-                            if noun in self.coref_dict.keys():
-                                if t not in self.coref_dict[noun]:
-                                    self.coref_dict[noun].append(t)
-                            else:
-                                self.coref_dict[noun] = [t]
+                        if re.search(syn, sentence, re.IGNORECASE):
+                            index = sentence.lower().find(syn.lower())
+                            matched_word = sentence[index:index + len(syn)]
+                            # Tuple of word and sentence number it came from
+                            t = (matched_word, loc)
+                            if t not in self.coref_resolved[noun]:
+                                self.coref_resolved[noun].append(t)
+
+    def tag_sentence(self):
+        for loc in self.coref_sentence.keys():
+            # Get the list of corefs in the current sentence
+            heads = self.coref_sentence[loc]
+            for noun in heads:
+                # Get the coref number
+                num = self.coref_index[noun]
+                # Only loop through the sentence the coref is in and onwards
+                for sentence in self.cleaned_sentences[loc:]:
+                    tokens = word_tokenize(sentence)
+                    tagged_sentence = pos_tag(tokens)
+                    tree = ne_chunk(tagged_sentence)
+                    # Check if coref is in the sentence
+                    index = sentence.find("X" + str(num))
+                    if index > 0:
+                        # Find where the coref begins in the sentence
+                        sentence = sentence[index + 3:]
+                    if re.search(noun, sentence, re.IGNORECASE):
+                        index = sentence.lower().find(noun.lower())
+                        matched_word = sentence[index:index + len(noun)]
+                        # Tuple of word and sentence number it came from
+                        t = (matched_word, loc)
+                        if t not in self.coref_resolved[noun]:
+                            self.coref_resolved[noun].append(t)
+
+    def fuzzy_string_match(self):
+        warnings.simplefilter("ignore")
+        # r = fuzz.ratio(str1, str2)
+        for loc in self.coref_sentence.keys():
+            # Get the list of corefs in the current sentence
+            heads = self.coref_sentence[loc]
+            for noun in heads:
+                # Get the coref number
+                num = self.coref_index[noun]
+                # Only loop through the sentence the coref is in and onwards
+                for sentence in self.cleaned_sentences[loc:]:
+                    # Check if coref is in the sentence
+                    index = sentence.find("X" + str(num))
+                    if index > 0:
+                        # Find where the coref begins in the sentence
+                        sentence = sentence[index + 3:]
+                    if re.search(noun, sentence, re.IGNORECASE):
+                        index = sentence.lower().find(noun.lower())
+                        matched_word = sentence[index:index + len(noun)]
+                        # Tuple of word and sentence number it came from
+                        t = (matched_word, loc)
+                        if t not in self.coref_resolved[noun]:
+                            self.coref_resolved[noun].append(t)
+
+    def fuzzy_synonym_match(self):
+        warnings.simplefilter("ignore")
+        for loc in self.coref_sentence.keys():
+            # Get the list of corefs in the current sentence
+            heads = self.coref_sentence[loc]
+            for noun in heads:
+                # Get the coref number
+                num = self.coref_index[noun]
+                # Build our nouns list of synonyms
+                synonyms = []
+                for syn in wordnet.synsets(noun):
+                    for l in syn.lemmas():
+                        if l.name() not in synonyms:
+                            synonyms.append(l.name().replace("_", " "))
+                # Only loop through the sentence the coref is in and onwards
+                for sentence in self.cleaned_sentences[loc:]:
+                    # Check if coref is in the sentence
+                    index = sentence.find("X" + str(num))
+                    if index > 0:
+                        # Find where the coref begins in the sentence
+                        sentence = sentence[index + 3:]
+                    for syn in synonyms:
+                        if re.search(syn, sentence, re.IGNORECASE):
+                            index = sentence.lower().find(syn.lower())
+                            matched_word = sentence[index:index + len(syn)]
+                            # Tuple of word and sentence number it came from
+                            t = (matched_word, loc)
+                            if t not in self.coref_resolved[noun]:
+                                self.coref_resolved[noun].append(t)
 
     def print_result(self):
-        for coref in self.corefs:
-            noun = coref[0]
-            index = coref[1]
-            print('<COREF ID="X%d">%s</COREF>' % (index, noun))
-            if noun not in self.coref_dict.keys():
+        for coref in self.coref_index.keys():
+            index = self.coref_index[coref]
+            print('<COREF ID="X%d">%s</COREF>' % (index, coref))
+            if coref not in self.coref_resolved.keys():
                 print()
                 continue
-            lists = self.coref_dict[noun]
+            lists = self.coref_resolved[coref]
             sortedWords = sorted(lists, key=lambda x: (x[1]))
             for word in sortedWords:
                 w_index = word[1]
@@ -154,14 +204,13 @@ class coref_file:
     def write_response(self):
         fullName = self.response + self.name + ".response"
         f = open(fullName, 'w')
-        for coref in self.corefs:
-            noun = coref[0]
-            index = coref[1]
-            f.write('<COREF ID="X%d">%s</COREF>\n' % (index, noun))
-            if noun not in self.coref_dict.keys():
+        for coref in self.coref_index.keys():
+            index = self.coref_index[coref]
+            f.write('<COREF ID="X%d">%s</COREF>\n' % (index, coref))
+            if coref not in self.coref_resolved.keys():
                 f.write('\n')
                 continue
-            lists = self.coref_dict[noun]
+            lists = self.coref_resolved[coref]
             sortedWords = sorted(lists, key=lambda x: (x[1]))
             for word in sortedWords:
                 w_index = word[1]
@@ -169,8 +218,6 @@ class coref_file:
                 f.write('{%d} {%s}\n' % (w_index, w_noun))
             f.write('\n')
         f.close()
-
-
 
 
 '''Takes in the name of a file, reads it, and returns a list of each line.'''
@@ -206,13 +253,11 @@ def main():
     '''
     #nlp = StanfordCoreNLP('/venv/Lib/site-packages/stanford-corenlp-full-2018-10-05/')
 
-
     list_file = "test.listfile"
-    response_dir = ""
+    response_dir = "responses/"
 
     # Get a list of all files we're reading
     input_files = parse_file_lines(list_file)
-
     # Create a coref object for each file
     for file_name in input_files:
         # Get all sentences
@@ -224,18 +269,19 @@ def main():
         c_file = coref_file(file_name, sentences, response_dir)
 
         c_file.find_corefs()
+        c_file.tag_sentence()
+        c_file.exact_string_match()
 
-        #c_file.exact_string_match()
-        c_file.string_match()
-        c_file.print_result()
+        c_file.exact_synonym_match()
 
-        c_file.synonym_match()
+        #c_file.fuzzy_synonym_match()
+
+        #c_file.fuzzy_string_match()
+
         c_file.print_result()
 
         #c_file.write_response()
 
-        #nltk.download('punkt')
-        #parser = CoreNLPParser(url='http://localhost:9000')
         #Tokenizer
         #parser.tokenize("some string")
         #POS Tagger
